b6ce80512757c761624d07e61ae15884a792f99f
Inbar Dagan <poupik@gmail.com>
2020-12-11 00:59:32 +0100

    Static logo

--- src/components/MetaTags.jsx
+++ src/components/MetaTags.jsx
@@ -22,9 +22,9 @@ function MetaTags(props) {
             <meta property="og:site_name" content={SITE_NAME}/>
 
             <meta property="og:url" content={url}/>
-            <meta property="og:image" content={logo} />
+            <meta property="og:image" content="https://raw.githubusercontent.com/inbar/einbuergerungstest-flash-cards-app/dev/src/resources/images/logo_wide.png" />
             <meta property="og:image:width" content="1200"/>
-            <meta property="og:image:height" content="1200"/>
+            <meta property="og:image:height" content="630"/>
             <meta property="og:description" content={props.description}/>
             
             {/* Twitter */}

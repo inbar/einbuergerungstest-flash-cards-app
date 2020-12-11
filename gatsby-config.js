2548c507a6bc099676c022e3fc7a71e6651e5c28
Inbar Dagan <poupik@gmail.com>
2020-12-11 22:47:10 +0100

    Add STAGE environment variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,34 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require('dotenv').config();
+
+const stage = process.env.STAGE ? process.env.STAGE : 'DEV';
+let siteName;
+let siteUrl;
+
+switch (stage) {
+    case 'PROD':
+        siteName = "Einbürgerungs Flash Cards App";
+        siteUrl = "https://www.einbuergerungstest-flash-cards.app";
+        break;
+    case 'BETA':
+        siteName = "Einbürgerungs Flash Cards App (BETA)";
+        siteUrl = "https://beta.einbuergerungstest-flash-cards.app";
+        break;
+    case 'GAMMA':
+        siteName = "Einbürgerungs Flash Cards App (GAMMA)";
+        siteUrl = "https://gamma.einbuergerungstest-flash-cards.app";
+        break;
+    default:
+        siteName = "Einbürgerungs Flash Cards App (DEV)";
+        siteUrl = "localhost:8000";
+}
+
 module.exports = {
     siteMetadata: {
-        name: "Einbürgerungs Flash Cards App",
-        url: "https://www.einbuergerungstest-flash-cards.app"
+        name: siteName,
+        url: siteUrl
     },
     /* Your site config here */
     plugins: [
